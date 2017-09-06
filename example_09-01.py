caccbfe4db21eb7a21119e34e14e9a8fc7dc93d6
David Owen <dro575@gmail.com>
2017-09-06 13:49:25 -0400

    Added example 9-1.

---
+++ example_09-01.py
@@ -0,0 +1,14 @@
+import cv2
+import sys
+
+if __name__ == '__main__':
+    cv2.namedWindow(sys.argv[1], cv2.WINDOW_AUTOSIZE)
+    img = cv2.imread(sys.argv[1])
+    cv2.imshow(sys.argv[1], img)
+    
+    while True:
+        if cv2.waitKey(100) == 27:
+            break
+    
+    cv2.destroyWindow(sys.argv[1])
+    exit(0)    
